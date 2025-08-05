20894cb0f8f3a68affa4bc51f29fd456020208f4
Yra <developer.ych@gmail.com>
2025-08-05 20:49:48 +0900

    Delete choicePoker directory

--- choicePoker/vitest-setup.ts
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom/vitest"; // 1
-import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
-import '@testing-library/jest-dom';
-
-afterEach(() => {
-  cleanup(); // 2
-});
